22957669c325dd426ef7837212b5366f50a4e05a
Aaron <aaron@Aarons-MacBook-Air.local>
2020-01-14 13:12:52 -0500

    fixed ban width issues

--- js/featuredProjects.js
+++ js/featuredProjects.js
@@ -1,17 +1,16 @@
 const card = document.querySelectorAll(".featured-bg .card");
-const video = document.querySelectorAll("video");
 
+//videos
 let V = [];
-
-V[0] = "./videos/CardClip.mp4";
+V[0] = "https://www.youtube.com/embed/KaaedNZTUos";
 V[1] = "";
 V[2] = "";
 V[3] = "";
 V[4] = "";
 V[5] = "";
 
+//links to github
 let L = [];
-
 L[0] = "https://github.com/Mental-Block/John-Doe-Masonry";
 L[1] = "";
 L[2] = "";
@@ -19,52 +18,68 @@ L[3] = "";
 L[4] = "";
 L[5] = "";
 
-const videoHover = (cardNumber, VideoNumber) => {
-  card[cardNumber].addEventListener("mouseover", () => {
-    video[VideoNumber].play();
+//live preview
+let P = [];
+P[0] = "https://johndoemasonry.netlify.com";
+P[1] = "";
+P[2] = "";
+P[3] = "";
+P[4] = "";
+P[5] = "";
 
-    card[cardNumber].addEventListener("mouseout", () => {
-      video[VideoNumber].pause();
-    });
-  });
-};
-
-const bigVideoPlayer = (cardNumber, videoNumber, link) => {
+const bigVideoPlayer = (cardNumber, videoNumber, linkNumber, siteNumber) => {
   card[cardNumber].addEventListener("click", () => {
-    let videoBox = document.createElement("div");
-    let videoBoxX = document.createElement("div");
-    let line1 = document.createElement("div");
-    let line2 = document.createElement("div");
-    let video1 = document.createElement("video");
-    let source = document.createElement("source");
-    let a = document.createElement("a");
-    let button = document.createElement("button");
+    const videoBox = document.createElement("div");
+    const videoBoxX = document.createElement("div");
+    const buttonBox = document.createElement("div");
+    const line1 = document.createElement("div");
+    const line2 = document.createElement("div");
+    const iframe = document.createElement("iframe");
+    const a = document.createElement("a");
+    const a2 = document.createElement("a");
+    const button = document.createElement("button");
+    const button2 = document.createElement("button");
 
     videoBox.classList.add("video-box");
     videoBoxX.classList.add("video-box-x");
+    buttonBox.classList.add("btn-box");
     line1.classList.add("line", "line1");
     line2.classList.add("line", "line2");
-    video1.classList.add("video1");
-    source.setAttribute("type", "video/mp4");
 
-    source.setAttribute("src", videoNumber);
-    a.setAttribute("href", link);
-    button.classList.add("btn");
+    iframe.classList.add("iframe");
+    iframe.setAttribute("src", videoNumber);
+    iframe.setAttribute("allowfullscreen", true);
+
+    a.setAttribute("href", linkNumber);
     a.setAttribute("target", "_blank");
+    a.classList.add("link");
+    button.innerHTML = "Source code";
+    button.classList.add("btn");
+
+    a2.setAttribute("href", siteNumber);
+    a2.setAttribute("target", "_blank");
+    a2.classList.add("link-1");
+    button2.innerHTML = "Live preview";
+    button2.classList.add("btn");
 
     document.getElementById("featured-projects").appendChild(videoBox);
     document.querySelector(".video-box").appendChild(videoBoxX);
     document.querySelector(".video-box-x").appendChild(line1);
     document.querySelector(".video-box-x").appendChild(line2);
-    document.querySelector(".video-box").appendChild(video1);
-    document.querySelector(".video-box .video1").appendChild(source);
-    document.querySelector(".video-box").appendChild(button);
-    document.querySelector(".video-box button").appendChild(a);
+    document.querySelector(".video-box").appendChild(iframe);
 
-    a.innerHTML = "Source code";
+    document.querySelector(".video-box").appendChild(buttonBox);
 
-    video1.controls = true;
-    video1.muted = true;
+    document.querySelector(".btn-box").appendChild(a);
+    document.querySelector(".link").appendChild(button);
+
+    document.querySelector(".btn-box").appendChild(a2);
+    document.querySelector(".link-1").appendChild(button2);
+
+    iframe.autoplay = false;
+    iframe.frameBorder = "0";
+    iframe.allow =
+      "accelerometer; encrypted-media; gyroscope; picture-in-picture";
 
     videoBoxX.addEventListener("click", () =>
       videoBox.parentNode.removeChild(videoBox)
@@ -72,22 +87,11 @@ const bigVideoPlayer = (cardNumber, videoNumber, link) => {
   });
 };
 
-bigVideoPlayer(0, V[0], L[0]);
+bigVideoPlayer(0, V[0], L[0], P[0]);
 /*
 bigVideoPlayer(1, V[1], L[1]);
 bigVideoPlayer(2, V[2], L[2]);
 bigVideoPlayer(3, V[3], L[3]);
 bigVideoPlayer(4, V[4], L[4]);
 bigVideoPlayer(5, V[5], L[5]);
-*/
-
-// Don't reference actual V[]; as video is already in the html
-videoHover(0, 0);
-
-/*
-videoHover(1, 1);
-videoHover(2, 2);
-videoHover(3, 3);
-videoHover(4, 4);
-videoHover(5, 5);
 */
